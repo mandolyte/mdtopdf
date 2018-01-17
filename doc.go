ed6e3de07c64f9de6ad1defb6fbc9cad426ed378
mandolyte <cecil.new@gmail.com>
2018-01-17 10:48:51 -0500

    doc fixes

--- doc.go
+++ doc.go
@@ -23,8 +23,10 @@ Package mdtopdf implements a PDF document generator for markdown documents.
 Introduction
 
 This package depends on two other packages:
+
 * The BlackFriday v2 parser to read the markdown source
-* The `gofpdf` packace to generate the PDF
+
+* The gofpdf packace to generate the PDF
 
 The tests included here are from the BlackFriday package.
 See the "testdata" folder.
@@ -38,14 +40,14 @@ and data provided in each while the AST is presented.
 
 Installation
 
-To install the package, run the usual `go get`:
+To install the package:
 
 	go get github.com/mandolyte/mdtopdf
 
 
 Quick start
 
-In the `cmd` folder is an example using the package. It demonstrates
+In the cmd folder is an example using the package. It demonstrates
 a number of features. The test PDF was created with this command:
 
 	go run convert.go -i test.md -o test.pdf
