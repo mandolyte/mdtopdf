74077c6f0d525c4a728a78f084eb91be0aeeb4d8
Jesse Portnoy <jesse@packman.io>
2023-07-22 19:43:58 +0100

    Support bullets

--- cmd/convert.go
+++ cmd/convert.go
@@ -51,13 +51,15 @@ func main() {
 	    pf = mdtopdf.NewPdfRenderer("", "", *output, "trace.log",
 		    mdtopdf.SetSyntaxHighlightBaseDir(*pathToSyntaxFiles))
 	}else{
-	    pf = mdtopdf.NewPdfRenderer("", "", *output, "trace.log")
+	    pf = mdtopdf.NewPdfRenderer("", "", *output, "trace.log",
+		    mdtopdf.SetSyntaxHighlightBaseDir("../highlight/syntax_files"))
 	}
 	pf.Pdf.SetSubject("How to convert markdown to PDF", true)
 	pf.Pdf.SetTitle("Example PDF converted from Markdown", true)
 	pf.THeader = mdtopdf.Styler{Font: "Times", Style: "IUB", Size: 20, Spacing: 2,
 		TextColor: mdtopdf.Color{Red: 0, Green: 0, Blue: 0},
 		FillColor: mdtopdf.Color{Red: 179, Green: 179, Blue: 255}}
+	pf.Normal = mdtopdf.Styler{Font: "Times", Style: "", Size: 12, Spacing: 2}
 	pf.TBody = mdtopdf.Styler{Font: "Arial", Style: "", Size: 12, Spacing: 2,
 		TextColor: mdtopdf.Color{Red: 0, Green: 0, Blue: 0},
 		FillColor: mdtopdf.Color{Red: 255, Green: 102, Blue: 129}}
